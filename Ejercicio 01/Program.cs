6f92e7e34d82d0b80f4d1664dba1ff6006ddee76
verno <verno@ASUS>
2026-03-22 20:46:48 -0600

    ej 1 completado

--- Ejercicio 01/Program.cs
+++ Ejercicio 01/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int edad;
+bool correcto;
+do 
+{
+    Console.WriteLine("Ingrese su edad");
+    correcto = int.TryParse(Console.ReadLine(), out edad);
+    if (!correcto )
+    {
+        Console.WriteLine("No es un valor numerico");
+        correcto = false;
+    }
+    else if (edad < 0 || edad > 110)
+    {
+        Console.WriteLine("La edad debe estar entre 0 y 110");
+        correcto = false;
+    }
+}
+while (!correcto || edad < 0 || edad > 110);
