eaca633b79ba41d7776fdda1a9de89ac939461be
verno <verno@ASUS>
2026-03-22 21:14:37 -0600

    ej 5 final

---
+++ Ejercicio 05/Program.cs
@@ -0,0 +1,28 @@
+﻿int numero;
+bool correcto;
+do
+{
+    Console.WriteLine("Ingrese un numero");
+    correcto = int.TryParse(Console.ReadLine(), out  numero);
+
+    if (!correcto)
+    {         Console.WriteLine("No es un valor numerico");
+        correcto = false;
+    }
+    else if (numero < 0)
+    {
+        Console.WriteLine("El numero no puede ser negativo");
+        correcto = false;
+
+    }
+}
+while (!correcto);
+
+if  (numero % 2 == 0)
+{
+    Console.WriteLine("El numero es par");
+}
+else
+{
+    Console.WriteLine("El numero es impar");
+} 
