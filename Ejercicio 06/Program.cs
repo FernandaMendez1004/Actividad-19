0473b30765e6515b4219926e1ec70723dd7fa69b
verno <verno@ASUS>
2026-03-22 21:24:17 -0600

    ej 06 final

---
+++ Ejercicio 06/Program.cs
@@ -0,0 +1,12 @@
+﻿DateTime fecha;
+bool correcta;
+do
+{
+    Console.WriteLine("Ingrese una fecha");
+    correcta = DateTime.TryParse(Console.ReadLine(), out fecha);
+    if (!correcta)
+    { Console.WriteLine("Fecha invalida"); }
+}
+while (!correcta);
+
+Console.WriteLine($"La fecha ingresada es: {fecha.ToShortDateString()}");
