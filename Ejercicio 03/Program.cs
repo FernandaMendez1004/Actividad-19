d522ca9f5052dd5826361d48beab47d60bf8ee89
verno <verno@ASUS>
2026-03-22 20:56:07 -0600

    ej 3 terminado

---
+++ Ejercicio 03/Program.cs
@@ -0,0 +1,18 @@
+﻿int estudiantes;
+bool correcto;
+
+do
+{
+    Console.WriteLine("Ingrese la cantidad de estudiantes");
+    correcto = int.TryParse(Console.ReadLine(), out estudiantes);
+    if (!correcto)
+    { Console.WriteLine("No es un valor numerico");
+        correcto = false;
+    }
+    else if (estudiantes < 0)
+    {
+        Console.WriteLine("La cantidad de estudiantes no puede ser negativa");
+        correcto = false;
+    }
+}
+while (!correcto);
