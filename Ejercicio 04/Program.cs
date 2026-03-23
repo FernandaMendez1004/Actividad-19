205bb8d9370eda3aed805714756730cfc1c0f96b
verno <verno@ASUS>
2026-03-22 21:05:10 -0600

    ej 3 terminado

---
+++ Ejercicio 04/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hello, World!");
+int year;
+bool correcto;
+
+do
+{
+    Console.WriteLine("Ingrese el año de su nacimiento");
+    correcto = int.TryParse(Console.ReadLine(), out year);
+    if (!correcto)
+    {
+        Console.WriteLine("No es un valor numerico");
+    }
+    else if (year < 1900 || year > 2026)
+    {
+        Console.WriteLine($"El año debe estar entre 1900 y {year}");
+        correcto = false;
+    }
+}
+while (!correcto);
