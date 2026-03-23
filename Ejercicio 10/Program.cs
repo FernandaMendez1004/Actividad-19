c87ebcfb4b7639e2cab1c57705a659fa48518f9c
verno <verno@ASUS>
2026-03-22 22:04:20 -0600

    ej 10

---
+++ Ejercicio 10/Program.cs
@@ -0,0 +1,28 @@
+﻿int productos;
+bool correcto;
+string nproductos;
+
+
+do
+{
+    Console.WriteLine("Ingrese la cantidad de productos:");
+    correcto = int.TryParse(Console.ReadLine(), out productos);
+
+    if (!correcto)
+    {
+        Console.WriteLine("Debe ser un valor numérico");
+    }
+    else if (productos <= 0)
+    {
+        Console.WriteLine("Debe ser un número entero positivo");
+        correcto = false;
+    }
+
+} while (!correcto);
+
+
+for (int i = 1; i <= productos; i++)
+{
+    Console.WriteLine($"Ingrese el nombre del producto ");
+    nproductos = Console.ReadLine();
+}
