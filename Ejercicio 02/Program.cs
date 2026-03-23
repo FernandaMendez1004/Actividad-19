09bf0bdd423194eef98abf3bfa3fc59343e2cfcc
verno <verno@ASUS>
2026-03-22 20:52:31 -0600

    ej 2 terminado

---
+++ Ejercicio 02/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hello, World!");
+double precio;
+bool correcto;
+do
+{
+    Console.WriteLine("Ingrese el precio");
+    correcto = double.TryParse(Console.ReadLine(), out precio);
+    if (!correcto)
+    {
+        Console.WriteLine("No es un valor numerico");
+        correcto = false;
+    }
+    else if (precio < 0  )
+    {
+        Console.WriteLine("El precio no puede ser negativo");
+        correcto = false;
+    }
+}
+while (!correcto);
